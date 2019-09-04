9150998d4a51085dc9bb5c490dec22111b9c218e
kanato4 <kntykt@icloud.com>
2019-09-04 21:11:39 +0900

    Update routes and controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :messages
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   root to: 'messages#index'
 end
