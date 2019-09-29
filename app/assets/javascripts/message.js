06d40881d46d4d932a25281a93cb313b599ced3e
kanato4 <kntykt@icloud.com>
2019-09-29 17:23:28 +0900

    Implemented asynchronous transmission

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,26 @@
 $(function(){
+
+  function buildMessage(message){
+    var img = message.image ? `<img src= ${message.image}` : "";
+    var html =`<div class="message">
+                <div class="message__upper-info">
+                  <div class="message__upper-info__talker">
+                    ${message.user_name}
+                  </div>
+                  <div class="message__upper-info__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="message__text">
+                  <p class="message__text__content">
+                    ${message.content}
+                  </p>
+                  ${img}
+                </div>
+               </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +32,14 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(message){
+      var html = buildMessage(message)
+      $('.messages').append(html);
+      $('.form__input-box').val('');
+    })
+    .fail(function(){
+      alert("エラー");
     })
   })
 });
